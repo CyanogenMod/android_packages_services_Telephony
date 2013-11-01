AOKP/packages_apps_Phone

Phone: IMS changes
- Place IMS call if the call domain is PS and the URI scheme is sip
- Add UI for adding IMS settings
- Fix merge call and add call support
- Fix missed PHONE_TYPE_IMS case
- Fix IMS phone to behave like GSM phone in CallNofifier.run
  TONE_BUSY case
- Use Ims Service to create IMS phone
- Handle Ims not registered in App
- Support for emergency calls over IMS
- Handle voLTE call end properly
- UT interface support for IMS
- Support for Conference Event Package UI
- Add support for manage conference when no conf event package
- IMS conference uri
- Expose information via ImsService
- While in 1x call, originate new call on 1x
- Handle ECBM for ImsPhone
- Move CallDetails constants to PhoneConstants
CRs-Fixed: 407196 421082 478436 478439 474115

Change-Id: If3af16a11c41bc351c8399bbc8d0bb1e3fb6c069

src/com/android/phone/EmergencyCallbackModeService.java
@@ -58,6 +58,7 @@ public class EmergencyCallbackModeService extends Service {
     private long mTimeLeft = 0;
     private Phone mPhone = null;
     private boolean mInEmergencyCall = false;
+    private boolean mIsImsPhone = false;
 
     private static final int ECM_TIMER_RESET = 1;
 
@@ -73,8 +74,27 @@ public class EmergencyCallbackModeService extends Service {
 
     @Override
     public void onCreate() {
-        // Check if it is CDMA phone
-        if (PhoneFactory.getDefaultPhone().getPhoneType() != PhoneConstants.PHONE_TYPE_CDMA) {
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+        PhoneGlobals app = PhoneGlobals.getInstance();
+
+        if (intent != null) {
+            mIsImsPhone = intent.getBooleanExtra("ims_phone", false);
+        } else {
+            Log.d(LOG_TAG, "onStartCommand: intent null");
+        }
+
+        if (mIsImsPhone) {
+            mPhone = PhoneUtils.getImsPhone(PhoneGlobals.getInstance().mCM);
+        } else {
+            mPhone = app.getPhone();
+        }
+
+        // Check if it is GSM phone, as GSM phone does not support ECBM
+        if (mPhone.getPhoneType() == PhoneConstants.PHONE_TYPE_GSM) {
             Log.e(LOG_TAG, "Error! Emergency Callback Mode not supported for " +
                     PhoneFactory.getDefaultPhone().getPhoneName() + " phones");
             stopSelf();
@@ -93,6 +113,7 @@ public class EmergencyCallbackModeService extends Service {
         mPhone.registerForEcmTimerReset(mHandler, ECM_TIMER_RESET, null);
 
         startTimerNotification();
+        return START_STICKY;
     }
 
     @Override
@@ -167,6 +188,9 @@ public class EmergencyCallbackModeService extends Service {
         Notification notification = new Notification(
                 R.drawable.picture_emergency25x25,
                 getText(R.string.phone_entered_ecm_text), 0);
+
+        Intent intent = new Intent(EmergencyCallbackModeExitDialog.ACTION_SHOW_ECM_EXIT_DIALOG);
+        intent.putExtra("ims_phone", mIsImsPhone);
 
         // PendingIntent to launch Emergency Callback Mode Exit activity if the user selects
         // this notification

src/com/qualcomm/ims/IImsServiceListener.aidl
@@ -1,0 +1,45 @@
+/*
+ * Copyright (c) 2013, The Linux Foundation. All rights reserved.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *  * Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above
+ *    copyright notice, this list of conditions and the following
+ *    disclaimer in the documentation and/or other materials provided
+ *    with the distribution.
+ *  * Neither the name of The Linux Foundation nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+
+ * THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+ * BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+ * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+ * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+
+package com.qualcomm.ims;
+
+/**
+ * Interface used for registering callbacks with IMS Service. {@hide}
+ */
+interface IImsServiceListener {
+
+    /**
+     * Get the changed ims registration state
+     */
+    void imsRegStateChanged(int regstate);
+
+    /**
+     * IMS registration state change request failure callback
+     */
+    void imsRegStateChangeReqFailed();
+}

CyanogenMod/android_packages_services_Telephony

- Copyright changes for IMS files
- Change Add Call functionality & Ecc call logic
- Added PHONE_TYPE_IMS to handle crash
- Fix UI for exception error scenarios for Supplementary srv setting
- Aidl changes for Hangup feature
- Fix not to strip separators for conference
- ECBM exit scenario
Change-Id: I0fcfd61e0e54d2dca601ac9db87ec40abc7748d4
CRs-Fixed: 407196 421082 478436 478439 474115 483481 491023
           445594 513188 519508
(cherry picked from commit d62c3e4f52dbf0fc3d506753549595405520225e)
(cherry picked from commit c5c2376cd17ee96d1c123644af2510f125dcd3c4)
(cherry picked from commit 367fea457afe8dec23dac1b5190edece0e045539)

src/com/android/phone/EmergencyCallbackModeService.java
@@ -58,6 +58,7 @@ public class EmergencyCallbackModeService extends Service {
     private long mTimeLeft = 0;
     private Phone mPhone = null;
     private boolean mInEmergencyCall = false;
+    private boolean mIsImsPhone = false;
 
     private static final int ECM_TIMER_RESET = 1;
 
@@ -73,8 +74,27 @@ public class EmergencyCallbackModeService extends Service {
 
     @Override
     public void onCreate() {
-        // Check if it is CDMA phone
-        if (PhoneFactory.getDefaultPhone().getPhoneType() != PhoneConstants.PHONE_TYPE_CDMA) {
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+        PhoneGlobals app = PhoneGlobals.getInstance();
+
+        if (intent != null) {
+            mIsImsPhone = intent.getBooleanExtra(PhoneGlobals.EXTRA_IMS_PHONE, false);
+        } else {
+            Log.e(LOG_TAG, "onStartCommand: intent null");
+        }
+
+        if (mIsImsPhone) {
+            mPhone = PhoneUtils.getImsPhone(PhoneGlobals.getInstance().mCM);
+        } else {
+            mPhone = app.getPhone();
+        }
+
+        // Check if it is GSM phone, as GSM phone does not support ECBM
+        if (mPhone.getPhoneType() == PhoneConstants.PHONE_TYPE_GSM) {
             Log.e(LOG_TAG, "Error! Emergency Callback Mode not supported for " +
                     PhoneFactory.getDefaultPhone().getPhoneName() + " phones");
             stopSelf();
@@ -93,6 +113,7 @@ public class EmergencyCallbackModeService extends Service {
         mPhone.registerForEcmTimerReset(mHandler, ECM_TIMER_RESET, null);
 
         startTimerNotification();
+        return START_STICKY;
     }
 
     @Override
@@ -168,10 +189,12 @@ public class EmergencyCallbackModeService extends Service {
                 R.drawable.picture_emergency25x25,
                 getText(R.string.phone_entered_ecm_text), 0);
 
+        Intent intent = new Intent(EmergencyCallbackModeExitDialog.ACTION_SHOW_ECM_EXIT_DIALOG);
+        intent.putExtra(PhoneGlobals.EXTRA_IMS_PHONE, mIsImsPhone);
+
         // PendingIntent to launch Emergency Callback Mode Exit activity if the user selects
         // this notification
-        PendingIntent contentIntent = PendingIntent.getActivity(this, 0,
-                new Intent(EmergencyCallbackModeExitDialog.ACTION_SHOW_ECM_EXIT_DIALOG), 0);
+        PendingIntent contentIntent = PendingIntent.getActivity(this, 0, intent, 0);
 
         // Format notification string
         String text = null;
@@ -238,5 +261,12 @@ public class EmergencyCallbackModeService extends Service {
      */
     public boolean getEmergencyCallbackModeCallState() {
         return mInEmergencyCall;
+    }
+
+    /**
+     * Returns true if ECBM is on IMS phone
+     */
+    public boolean isEcbmOnIms() {
+        return mIsImsPhone;
     }
 }

src/org/codeaurora/ims/IImsServiceListener.aidl
@@ -1,0 +1,45 @@
+/*
+ * Copyright (c) 2013, The Linux Foundation. All rights reserved.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *  * Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above
+ *    copyright notice, this list of conditions and the following
+ *    disclaimer in the documentation and/or other materials provided
+ *    with the distribution.
+ *  * Neither the name of The Linux Foundation nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+
+ * THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+ * BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+ * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+ * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+
+package org.codeaurora.ims;
+
+/**
+ * Interface used for registering callbacks with IMS Service. {@hide}
+ */
+interface IImsServiceListener {
+
+    /**
+     * Get the changed ims registration state
+     */
+    void imsRegStateChanged(int regstate);
+
+    /**
+     * IMS registration state change request failure callback
+     */
+    void imsRegStateChangeReqFailed();
+}