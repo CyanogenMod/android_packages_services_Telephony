23617c02049a226aaee7f194b8b3fe2683aa0e13
Suchand Ghosh <suchan@codeaurora.org>
2013-12-02 12:47:46 +0530

    CSVT: Introduce Circuit-Switch Video Telephony.
    Introduce CSVT Service which provides necessary API
    for Circuit-Switch Video Telephony.
    
    Change-Id: Ib74a6c4fb78dd6eb0a98d854b9e7c19e5bbc487e
    CRs-Fixed: 585497

--- src/com/android/phone/PhoneGlobals.java
+++ src/com/android/phone/PhoneGlobals.java
@@ -79,6 +79,7 @@ import com.android.services.telephony.common.AudioMode;
 
 import org.codeaurora.ims.IImsService;
 import static com.android.internal.telephony.MSimConstants.DEFAULT_SUBSCRIPTION;
+import org.codeaurora.ims.csvt.ICsvtService;
 
 /**
  * Global state for the telephony subsystem when running in the primary
@@ -198,6 +199,7 @@ public class PhoneGlobals extends ContextWrapper implements WiredHeadsetListener
     static boolean sVoiceCapable = true;
 
     public static IImsService mImsService;
+    public static ICsvtService mCsvtService;
 
     // Internal PhoneApp Call state tracker
     CdmaPhoneCallState cdmaPhoneCallState;
@@ -419,6 +421,8 @@ public class PhoneGlobals extends ContextWrapper implements WiredHeadsetListener
             mCM.registerPhone(phone);
 
             createImsService();
+
+            createCsvtService();
 
             // Create the NotificationMgr singleton, which is used to display
             // status bar icons and control other status bar behavior.
@@ -643,6 +647,34 @@ public class PhoneGlobals extends ContextWrapper implements WiredHeadsetListener
             Log.w(LOG_TAG,"Ims Service onServiceDisconnected");
         }
     };
+
+    public void createCsvtService() {
+        if (PhoneUtils.isCallOnCsvtEnabled()) {
+            try {
+                Intent intent = new Intent("org.codeaurora.ims.csvt.ICsvtService");
+                boolean bound = bindService(intent,
+                        mCsvtServiceConnection, Context.BIND_AUTO_CREATE);
+                Log.d(LOG_TAG, "ICsvtService bound request : " + bound);
+            } catch (NoClassDefFoundError e) {
+                //csvt is not supported so ignore creating csvt service.
+                Log.w(LOG_TAG, "Ignoring ICsvtService class not found exception "
+                        + e);
+            }
+        }
+    }
+
+    private static ServiceConnection mCsvtServiceConnection = new ServiceConnection() {
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            mCsvtService = ICsvtService.Stub.asInterface(service);
+            Log.d(LOG_TAG,"Csvt Service Connected: " + mCsvtService);
+        }
+
+        public void onServiceDisconnected(ComponentName arg0) {
+            Log.w(LOG_TAG,"Csvt Service onServiceDisconnected");
+            mCsvtService = null;
+        }
+    };
+
 
     /**
      * Returns the singleton instance of the PhoneApp.
--- src/com/android/phone/PhoneUtils.java
+++ src/com/android/phone/PhoneUtils.java
@@ -3099,6 +3099,13 @@ public class PhoneUtils {
     }
 
     /**
+     * Returns true if Android supports Csvt calls
+     */
+    public static boolean isCallOnCsvtEnabled() {
+        return CallManager.isCallOnCsvtEnabled();
+    }
+
+    /**
      * If the intent is not  already the IMS intent, conert the intent to the
      * IMS intent
      */
@@ -3349,6 +3356,17 @@ public class PhoneUtils {
             }
         }
         return otherSub;
+    }
+
+    public static boolean isCsvtCallActive() {
+        boolean isActive = false;
+        try {
+            isActive =  PhoneGlobals.mCsvtService != null &&
+                      ! PhoneGlobals.mCsvtService.isIdle();
+        } catch (RemoteException e) {
+            Log.e(LOG_TAG, "Failed to retrieve Csvt call state. " + e);
+        }
+        return isActive;
     }
 
     public static void handleWaitingCallOnLchSub(int activeSub, boolean isAccepted) {
