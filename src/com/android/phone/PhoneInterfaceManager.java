8b989a0bfd5ef82ed455f04b5025cf326edae45a
Scott Warner <Tortel1210@gmail.com>
2014-01-22 23:45:48 -0500

    Implement toggleLte
    Change-Id: I2bab17ecda3454fc1d405172c21005497ea8fa1b

--- src/com/android/phone/PhoneInterfaceManager.java
+++ src/com/android/phone/PhoneInterfaceManager.java
@@ -64,6 +64,7 @@ public class PhoneInterfaceManager extends ITelephony.Stub {
     private static final int CMD_ANSWER_RINGING_CALL = 4;
     private static final int CMD_END_CALL = 5;  // not used yet
     private static final int CMD_SILENCE_RINGER = 6;
+    private static final int CMD_TOGGLE_LTE = 7; // not used yet
 
     /** The singleton instance. */
     private static PhoneInterfaceManager sInstance;
@@ -298,7 +299,34 @@ public class PhoneInterfaceManager extends ITelephony.Stub {
     }
 
     public void toggleLTE(boolean on) {
-        return;
+        int network = -1;
+//        boolean usesQcLte = SystemProperties.getBoolean(
+//                        "ro.config.qc_lte_network_modes", false);
+
+        if (getLteOnGsmMode() != 0) {
+            if (on) {
+                network = Phone.NT_MODE_LTE_CMDA_EVDO_GSM_WCDMA;
+            } else {
+                network = Phone.NT_MODE_WCDMA_PREF;
+            }
+//        } else if (usesQcLte) {
+//            if (on) {
+//                network = Phone.NT_MODE_LTE_CMDA_EVDO_GSM_WCDMA;
+//            } else {
+//                network = Phone.NT_MODE_CDMA;
+//            }
+        } else {
+            if (on) {
+                network = Phone.NT_MODE_LTE_CMDA_EVDO_GSM_WCDMA;
+            } else {
+                network = Phone.NT_MODE_CDMA;
+            }
+        }
+
+        mPhone.setPreferredNetworkType(network,
+                mMainThreadHandler.obtainMessage(CMD_TOGGLE_LTE));
+        android.provider.Settings.Global.putInt(mApp.getContentResolver(),
+                android.provider.Settings.Global.PREFERRED_NETWORK_MODE, network);
     }
 
     private boolean showCallScreenInternal(boolean specifyInitialDialpadState,
