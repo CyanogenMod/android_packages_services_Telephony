8fd1290825ba3ad767bac9657ceeb9ac6fbd50e0
kaiyiz <kaiyiz@codeaurora.org>
2014-03-24 11:19:55 +0800

    Settings: Display error when MO a call in airplane mode without SIMcard
    When without SIM card inserted, service state cann't be
    "STATE_IN_SERVICE", so cause cann't dismiss the dialog.
    
    Add the judgement that has IccCard or not. First judge has IccCard,
    if has not, pop up "Mobile network not available" dialog. If has
    IccCard, then judge service state to work.
    
    CRs-Fixed: 633688
    
    Change-Id: Iffc46f1125c535e664857888a742acd5fd7e411c

--- src/com/android/phone/CallController.java
+++ src/com/android/phone/CallController.java
@@ -37,6 +37,7 @@ import android.provider.CallLog.Calls;
 import android.telephony.MSimTelephonyManager;
 import android.telephony.PhoneNumberUtils;
 import android.telephony.ServiceState;
+import android.telephony.TelephonyManager;
 import android.util.Log;
 
 import static com.android.internal.telephony.MSimConstants.SUBSCRIPTION_KEY;
@@ -597,6 +598,20 @@ public class CallController extends Handler {
         }
     }
 
+    private boolean hasIccCard() {
+        if (MSimTelephonyManager.getDefault().isMultiSimEnabled()) {
+            MSimTelephonyManager mSimTelephonyManager = MSimTelephonyManager.getDefault();
+            for (int i = 0; i < mSimTelephonyManager.getPhoneCount(); i++)
+                if (mSimTelephonyManager.hasIccCard(i))
+                    return true;
+        } else {
+            TelephonyManager telephonyManager = TelephonyManager.getDefault();
+            if (telephonyManager.hasIccCard())
+                return true;
+        }
+        return false;
+    }
+
     /**
      * Checks the current ServiceState to make sure it's OK
      * to try making an outgoing call to the specified number.
@@ -615,6 +630,9 @@ public class CallController extends Handler {
                 return CallStatusCode.SUCCESS;
 
             case ServiceState.STATE_POWER_OFF:
+                if (!hasIccCard()) {
+                    return CallStatusCode.OUT_OF_SERVICE;
+                }
                 // Radio is explictly powered off.
                 return CallStatusCode.POWER_OFF;
 
