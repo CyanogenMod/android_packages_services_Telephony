fc218af348673f8ebfa138724c73cb4386281ed0
Steve Kondik <shade@chemlab.org>
2014-04-13 23:32:09 -0700

    telephony: Disable MSIM settings if unavailable
     * These activities share the same intent-filter as their non-MSIM
       counterparts. While this makes sense as a quick hack, it causes
       a UX issue when an app tries to launch one of these. An example
       is Google Voice- when it's time to update your voicemail settings,
       you're confusingly presented with a dialog asking to select
       which "com.android.phone" to use.
     * Disable the activities entirely if MSIM isn't available.
    
    Change-Id: Iedeaf84f343ea0ee290275941cd55862a687800a

---
+++ src/com/android/phone/MSimActivityDisabler.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2014 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.phone;
+
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.telephony.MSimTelephonyManager;
+
+public class MSimActivityDisabler extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        boolean enabled = MSimTelephonyManager.getDefault().isMultiSimEnabled();
+
+        // These activities share actions in their intent filters. We need to select the correct one
+        // so that apps get what they are looking for, depending if MSIM is available or not.
+        if (enabled) {
+            disable(context, CallFeaturesSetting.class);
+            disable(context, MobileNetworkSettings.class);
+        } else {
+            disable(context, MSimCallFeaturesSetting.class);
+            disable(context, MSimMobileNetworkSettings.class);
+        }
+
+        // Die an honorable death, our work is done here.
+        disable(context, this.getClass());
+    }
+
+    private void disable(Context context, Class<?> klass) {
+        PackageManager pm = context.getPackageManager();
+        ComponentName component = new ComponentName(context, klass);
+        pm.setComponentEnabledSetting(component, PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
+                PackageManager.DONT_KILL_APP);
+    }
+}
