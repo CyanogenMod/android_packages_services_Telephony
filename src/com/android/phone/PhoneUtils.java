6566e475ba55bc1989e58f77264d2b3d1f132049
Adnan <adnan@cyngn.com>
2014-06-09 16:02:53 -0700

    Chinese Location Lookup [2/2] Telephony: Detect external plugin.
        -PS3: Fix installed package logic.
    
    Change-Id: I9ee3f925af09a5689ad7b72a2f1c2e1925fa3433

--- src/com/android/phone/PhoneUtils.java
+++ src/com/android/phone/PhoneUtils.java
@@ -29,6 +29,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.res.Configuration;
 import android.graphics.drawable.Drawable;
@@ -3619,5 +3620,21 @@ public class PhoneUtils {
         }
         if (DBG) log("isImsVtCallNotAllowed: " + isNotAllowed);
         return isNotAllowed;
+    }
+
+    public static boolean isPackageInstalled(Context context, String pkg) {
+        if (pkg == null) {
+            return false;
+        }
+        try {
+            PackageInfo pi = context.getPackageManager().getPackageInfo(pkg, 0);
+            if (!pi.applicationInfo.enabled) {
+                return false;
+            } else {
+                return true;
+            }
+        } catch (NameNotFoundException e) {
+            return false;
+        }
     }
 }
