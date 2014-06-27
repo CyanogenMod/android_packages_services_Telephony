d28f32517815622c56b7bf0e254b0dcb5c9fbaec
Dan Pasanen <dan.pasanen@gmail.com>
2014-06-27 08:23:33 -0500

    toggleLTE: switch to Global mode on devices that default to it
    Verizon devices default to Global mode. When the LTE toggle is
    flipped back on, these devices should go to Global(10) instead
    of LTE/CDMA/EVDO(8)
    
    Change-Id: Id8e44bbca09d46b6622c72627f7dbd3c1ad4b678

--- src/com/android/phone/PhoneInterfaceManager.java
+++ src/com/android/phone/PhoneInterfaceManager.java
@@ -37,6 +37,7 @@ import android.os.Message;
 import android.os.Process;
 import android.os.RemoteException;
 import android.os.ServiceManager;
+import android.os.SystemProperties;
 import android.os.UserHandle;
 import android.preference.PreferenceManager;
 import android.provider.Settings;
@@ -858,7 +859,12 @@ public class PhoneInterfaceManager extends ITelephony.Stub {
             break;
         // CDMA Devices
         case Phone.NT_MODE_CDMA:
-            network = Phone.NT_MODE_LTE_CDMA_AND_EVDO;
+            if (SystemProperties.getInt("ro.telephony.default_network", 0) ==
+                        RILConstants.NETWORK_MODE_LTE_CMDA_EVDO_GSM_WCDMA) {
+                network = Phone.NT_MODE_LTE_CMDA_EVDO_GSM_WCDMA;
+            } else {
+                network = Phone.NT_MODE_LTE_CDMA_AND_EVDO;
+            }
             break;
         case Phone.NT_MODE_LTE_CDMA_AND_EVDO:
             network = Phone.NT_MODE_CDMA;
