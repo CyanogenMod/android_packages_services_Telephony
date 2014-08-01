b4a8d2fe35062865f91e1ddd42c6f7fc8d5b5723
Danny Baumann <dannybaumann@web.de>
2014-08-01 10:04:43 +0200

    Don't overzealously disable all preferences in airplane mode.
    Only disable those prefs that actually need the network connection.
    Fixes applying various preference summaries.
    
    Change-Id: Id89a2572e382b28c1da100ebb1a1a73eb9233e35

--- src/com/android/phone/CallFeaturesSetting.java
+++ src/com/android/phone/CallFeaturesSetting.java
@@ -251,6 +251,17 @@ public class CallFeaturesSetting extends PreferenceActivity
         new Locale("he"), new Locale("zh")
     };
 
+    // keys of preferences which should be disabled in airplane mode
+    private static final List<String> DISABLE_IN_AIRPLANE_MODE_PREFS = Arrays.asList(new String[] {
+        BUTTON_VOICEMAIL_PROVIDER_KEY, BUTTON_VOICEMAIL_SETTING_KEY,
+        BUTTON_GSM_UMTS_OPTIONS, BUTTON_CDMA_OPTIONS,
+        BUTTON_TTY_KEY, BUTTON_FDN_KEY,
+        // GSM specific options
+        "button_cf_expand_key", "button_more_expand_key",
+        // CDMA specific options
+        "button_voice_privacy_key"
+    });
+
     private Phone mPhone;
 
     private AudioManager mAudioManager;
@@ -1938,14 +1949,7 @@ public class CallFeaturesSetting extends PreferenceActivity
         mForeground = true;
 
         if (isAirplaneModeOn()) {
-            Preference sipSettings = findPreference(SIP_SETTINGS_CATEGORY_KEY);
-            PreferenceScreen screen = getPreferenceScreen();
-            int count = screen.getPreferenceCount();
-            for (int i = 0 ; i < count ; ++i) {
-                Preference pref = screen.getPreference(i);
-                if (pref != sipSettings) pref.setEnabled(false);
-            }
-            return;
+            disablePreferencesForAirplaneMode(getPreferenceScreen());
         }
 
         if (mVibrateWhenRinging != null) {
@@ -2005,6 +2009,18 @@ public class CallFeaturesSetting extends PreferenceActivity
         restoreLookupProviders();
     }
 
+    private void disablePreferencesForAirplaneMode(PreferenceGroup group) {
+        int count = group.getPreferenceCount();
+        for (int i = 0 ; i < count ; ++i) {
+            Preference pref = group.getPreference(i);
+            if (DISABLE_IN_AIRPLANE_MODE_PREFS.contains(pref.getKey())) {
+                pref.setEnabled(false);
+            } else if (pref instanceof PreferenceGroup) {
+                disablePreferencesForAirplaneMode((PreferenceGroup) pref);
+            }
+        }
+    }
+
     private void updateBlacklistSummary() {
         if (mButtonBlacklist != null) {
             if (BlacklistUtils.isBlacklistEnabled(this)) {
@@ -2057,8 +2073,8 @@ public class CallFeaturesSetting extends PreferenceActivity
     }
 
     private boolean isAirplaneModeOn() {
-        return Settings.System.getInt(getContentResolver(),
-                Settings.System.AIRPLANE_MODE_ON, 0) != 0;
+        return Settings.Global.getInt(getContentResolver(),
+                Settings.Global.AIRPLANE_MODE_ON, 0) != 0;
     }
 
     private void saveT9SearchInputLocale(Preference preference, String newT9Locale) {
