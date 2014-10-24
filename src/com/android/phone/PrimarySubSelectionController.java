3579c5dbb9e9069946dd2e977c9164fd3b6c7ddd
kaiyiz <kaiyiz@codeaurora.org>
2014-10-24 17:57:31 +0800

    TeleService: Carrier name not map with the language and the carrier
    Carrier name not map with language and use the wrong subId to get
    the carrier name.
    
    Add multi-language support and use the correct parameter.
    
    CRs-Fixed: 739302
    
    Change-Id: I4c429fadabc1460578def62ea17c4bb752e72979

--- src/com/android/phone/PrimarySubSelectionController.java
+++ src/com/android/phone/PrimarySubSelectionController.java
@@ -317,7 +317,10 @@ public class PrimarySubSelectionController extends Handler implements OnClickLis
         if (uiccCard != null && uiccCard.getCardState() == CardState.CARDSTATE_ABSENT) {
             return mContext.getString(R.string.sim_absent);
         } else {
-            String carrierName = TelephonyManager.getDefault().getSimOperatorName(slot);
+            String carrierName = TelephonyManager.getDefault().getSimOperatorName(
+                    SubscriptionManager.getSubId(slot)[0]);
+            carrierName = NativeTextHelper.getInternalLocalString(mContext, carrierName,
+                    R.array.origin_carrier_names, R.array.locale_carrier_names);
             if (TextUtils.isEmpty(carrierName) || TextUtils.isDigitsOnly(carrierName)) {
                 String iccId = mCardStateMonitor.getIccId(slot);
                 String spn = IINList.getDefault(mContext).getSpn(iccId);
