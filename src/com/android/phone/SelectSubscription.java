997e6a73f75fe1d5d3760964ddd0de265b97b172
cretin45 <cretin45@gmail.com>
2014-10-30 15:56:52 -0700

    TeleService: Refactor sim name / sim label logic
    Change-Id: I63a0a2f4d03b91289354cd9d6b51495cd622f760

--- src/com/android/phone/SelectSubscription.java
+++ src/com/android/phone/SelectSubscription.java
@@ -35,6 +35,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.telephony.MSimTelephonyManager;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.MenuItem;
 import android.widget.TabHost;
@@ -92,13 +93,16 @@ public class SelectSubscription extends  TabActivity {
         int numPhones = tm.getPhoneCount();
 
         for (int i = 0; i < numPhones; i++) {
-            String operatorName = tm.getSimOperatorName(i);
-            String label;
-            if (tm.getSimState(i) == SIM_STATE_ABSENT || tm.getSimState(i) != SIM_STATE_READY ||
-                    operatorName == null || operatorName.length() == 0) {
-                label = getString(R.string.multi_sim_entry_format_no_carrier, i + 1);
+            String label = Settings.Global.getSimNameForSubscription(this, i, null);
+            if (TextUtils.isEmpty(label)) {
+                String operatorName = tm.getSimOperatorName(i);
+                if (tm.getSimState(i) == SIM_STATE_ABSENT || TextUtils.isEmpty(operatorName)) {
+                    label = getString(R.string.default_sim_name, i + 1);
+                } else {
+                    label = getString(R.string.multi_sim_entry_format, operatorName, i + 1);
+                }
             } else {
-                label = getString(R.string.multi_sim_entry_format, operatorName, i + 1);
+                label = getString(R.string.multi_sim_entry_format, label, i + 1);
             }
             subscriptionPref = tabHost.newTabSpec(label);
             subscriptionPref.setIndicator(label);
