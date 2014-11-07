dcc3a83e64a005894408e8d9b431068526c66091
Adnan <adnan@cyngn.com>
2014-11-06 16:41:33 -0800

    Telephony: Implement protected sms broadcast API.
      - Normalize any addresses to be added or removed.
      - Enforce MODIFY_PROTECTED_SMS_LIST permission.
    
    Change-Id: Ieb0cb46be6ce107297988f6035a0a13d0db2c850

--- src/com/android/phone/PhoneInterfaceManager.java
+++ src/com/android/phone/PhoneInterfaceManager.java
@@ -19,6 +19,7 @@
 
 package com.android.phone;
 
+import android.Manifest;
 import android.app.ActivityManager;
 import android.app.AppOpsManager;
 import android.bluetooth.IBluetoothHeadsetPhone;
@@ -63,6 +64,7 @@ import com.android.internal.util.HexDump;
 import com.android.internal.telephony.uicc.IccIoResult;
 import com.android.internal.telephony.uicc.IccUtils;
 import com.android.internal.telephony.CommandException;
+import com.google.i18n.phonenumbers.PhoneNumberUtil;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -1049,6 +1051,74 @@ public class PhoneInterfaceManager extends ITelephony.Stub implements CallModele
         mPhone.setCellInfoListRate(rateInMillis);
     }
 
+
+    /**
+     * Allows an application to add a protected sms address if the application has
+     * been granted the permission MODIFY_PROTECTED_SMS_LIST.
+     * @param address
+     * @hide
+     */
+    @Override
+    public void addProtectedSmsAddress(String address) {
+        // Enforce MODIFY_PROTECTED_SMS_LIST permission
+        // requires the application to be signature
+        enforceModifyProtectedSms();
+
+        if (TextUtils.isEmpty(address)) {
+            return;
+        }
+
+        //Normalize the number
+        String normalized = PhoneNumberUtil.normalizeDigitsOnly(address);
+
+        List<String> settings =
+                Settings.Secure.getDelimitedStringAsList(mApp.getContentResolver(),
+                        Settings.Secure.PROTECTED_SMS_ADDRESSES, "\\|");
+        if (!settings.contains(normalized)) {
+            // Add the address
+            settings.add(normalized);
+        }
+
+        // Commit
+        Settings.Secure.putString(mApp.getContentResolver(),
+                Settings.Secure.PROTECTED_SMS_ADDRESSES, TextUtils.join("|", settings));
+    }
+
+    /**
+     * Allows an application to revoke/remove a protected sms address if the application has been
+     * granted the permission MODIFY_PROTECTED_SMS_LIST.
+     * @param address
+     * @return true if address is successfully removed
+     * @hide
+     */
+    @Override
+    public boolean revokeProtectedSmsAddress(String address) {
+        // Enforce MODIFY_PROTECTED_SMS_LIST permission
+        // requires the application to be signature
+        enforceModifyProtectedSms();
+
+        if (TextUtils.isEmpty(address)) {
+            return false;
+        }
+
+        //Normalize the number
+        String normalized = PhoneNumberUtil.normalizeDigitsOnly(address);
+
+        List<String> settings =
+                Settings.Secure.getDelimitedStringAsList(mApp.getContentResolver(),
+                        Settings.Secure.PROTECTED_SMS_ADDRESSES, "\\|");
+
+        if (settings.contains(normalized)) {
+            settings.remove(normalized);
+            // Commit
+            Settings.Secure.putString(mApp.getContentResolver(),
+                    Settings.Secure.PROTECTED_SMS_ADDRESSES, TextUtils.join("\\|", settings));
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     //
     // Internal helper methods.
     //
@@ -1101,6 +1171,16 @@ public class PhoneInterfaceManager extends ITelephony.Stub implements CallModele
      */
     private void enforceModifyPermission() {
         mApp.enforceCallingOrSelfPermission(android.Manifest.permission.MODIFY_PHONE_STATE, null);
+    }
+
+    /**
+     * Make sure the caller has the MODIFY_PROTECTED_SMS_LIST permission
+     *
+     * @throws SecurityException if the caller does not have the required permission
+     */
+    private void enforceModifyProtectedSms() {
+        mApp.enforceCallingOrSelfPermission(
+                android.Manifest.permission.MODIFY_PROTECTED_SMS_LIST, null);
     }
 
     /**
