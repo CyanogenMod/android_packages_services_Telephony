95c152958a7fd9f5e774c3ddbcfabb006d047cf9
Preeti Ahuja <preetia@codeaurora.org>
2014-11-07 15:05:52 -0800

    Disable manual network selection in Global modes.
    For multimode targets disable manual network selection
    and force automatic mode network selection
    in Global and Global+LTE modes.
    Global Pre-FIT Test Cases: 5.6.4, 5.6.5, 5.6.8
    
    Change-Id: I3d09d4beb7c2cf2e04d3c67beefff292ad28b4c2
    CRs-Fixed: 735057

--- src/com/android/phone/GsmUmtsOptions.java
+++ src/com/android/phone/GsmUmtsOptions.java
@@ -135,7 +135,10 @@ public class GsmUmtsOptions {
         Resources res = mPrefActivity.getResources();
 
         enablePlmnIncSearch();
-        if (res.getBoolean(R.bool.csp_enabled)) {
+        if(!mPhone.isManualNetSelAllowed()) {
+            log("Manual network selection not allowed.Disabling Operator Selection menu.");
+            mPrefScreen.removePreference(mButtonOperatorSelectionExpand);
+        } else if (res.getBoolean(R.bool.csp_enabled)) {
             if (mPhone.isCspPlmnEnabled()) {
                 log("[CSP] Enabling Operator Selection menu.");
                 mButtonOperatorSelectionExpand.setEnabled(true);
@@ -144,13 +147,16 @@ public class GsmUmtsOptions {
                 mPrefScreen.removePreference(mButtonOperatorSelectionExpand);
             }
         }
-        // Read platform settings for carrier settings
-        final boolean isCarrierSettingsEnabled = mPrefActivity.getResources().getBoolean(
-                R.bool.config_carrier_settings_enable);
-        if (!isCarrierSettingsEnabled) {
-            Preference pref = mPrefScreen.findPreference(BUTTON_CARRIER_SETTINGS_KEY);
-            if (pref != null) {
-                mPrefScreen.removePreference(pref);
+
+        if (res.getBoolean(R.bool.csp_enabled)) {
+            // Read platform settings for carrier settings
+            final boolean isCarrierSettingsEnabled = mPrefActivity.getResources().getBoolean(
+                    R.bool.config_carrier_settings_enable);
+            if (!isCarrierSettingsEnabled) {
+                Preference pref = mPrefScreen.findPreference(BUTTON_CARRIER_SETTINGS_KEY);
+                if (pref != null) {
+                    mPrefScreen.removePreference(pref);
+                }
             }
         }
         if (!mRemovedAPNExpand) {
