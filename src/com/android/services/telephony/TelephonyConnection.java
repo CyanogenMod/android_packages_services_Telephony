186eb014fb501af90a15e642fba4e118f87cf295
Andrew Lee <anwlee@google.com>
2014-11-06 17:07:12 -0800

    Disable MANAGE_CONFERENCE in TelephonyConference.
    This used to be in TelephonyConnection, but it wasn't called properly
    there, and didn't make as much conceptual sense there.
    
    Bug: 18241753
    Change-Id: Iab52392c7c183255951636e735979d1e6b6ca9d5

--- src/com/android/services/telephony/TelephonyConnection.java
+++ src/com/android/services/telephony/TelephonyConnection.java
@@ -688,25 +688,6 @@ abstract class TelephonyConnection extends Connection {
         }
     }
 
-    @Override
-    public void onConferenceChanged() {
-        Conference conference = getConference();
-        if (conference == null) {
-            return;
-        }
-
-        // If the conference was an IMS connection currently or before, disable MANAGE_CONFERENCE
-        // as the default behavior. If there is a conference event package, this may be overridden.
-        if (mWasImsConnection) {
-            int capabilities = conference.getCapabilities();
-            if (PhoneCapabilities.can(capabilities, PhoneCapabilities.MANAGE_CONFERENCE)) {
-                int newCapabilities =
-                        PhoneCapabilities.remove(capabilities, PhoneCapabilities.MANAGE_CONFERENCE);
-                conference.setCapabilities(newCapabilities);
-            }
-        }
-    }
-
     public void performHold() {
         Log.v(this, "performHold");
         // TODO: Can dialing calls be put on hold as well since they take up the
