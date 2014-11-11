2aee0176c52b6d88dabf740f5fe5263c42632a04
Jay Shrauner <shrauner@google.com>
2014-11-11 15:19:58 -0800

    Fix NPE in onCreate
    Use TextUtils.equals for null-safe string comparison
    
    Bug:18337966
    Conflicts:
    	src/com/android/phone/CallFeaturesSetting.java
    
    Change-Id: I19b39c43636e04fcefabccdfff097bb3383f088f

--- src/com/android/phone/CallFeaturesSetting.java
+++ src/com/android/phone/CallFeaturesSetting.java
@@ -1661,7 +1661,7 @@ public class CallFeaturesSetting extends PreferenceActivity
         // Show the voicemail preference in onResume if the calling intent specifies the
         // ACTION_ADD_VOICEMAIL action.
         mShowVoicemailPreference = (icicle == null) &&
-                getIntent().getAction().equals(ACTION_ADD_VOICEMAIL);
+                TextUtils.equals(getIntent().getAction(), ACTION_ADD_VOICEMAIL);
     }
 
     private void initPhoneAccountPreferences() {
