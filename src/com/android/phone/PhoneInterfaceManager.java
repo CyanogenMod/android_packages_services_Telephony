051173cb718cfac253106ad2677be7ab2d43e6fc
Amit Mahajan <amitmahajan@google.com>
2014-11-18 11:54:45 -0800

    Fixing a comment (changing phoneId to subId)
    Conflicts:
    	src/com/android/phone/PhoneInterfaceManager.java
    Bug: 18424733
    Change-Id: Ib1c6d55c5bf2cff8986c740e96881f34e536b43f

--- src/com/android/phone/PhoneInterfaceManager.java
+++ src/com/android/phone/PhoneInterfaceManager.java
@@ -1913,7 +1913,7 @@ public class PhoneInterfaceManager extends ITelephony.Stub {
     @Override
     public int getCalculatedPreferredNetworkType() {
         enforceReadPermission();
-        return PhoneFactory.calculatePreferredNetworkType(mPhone.getContext());
+        return PhoneFactory.calculatePreferredNetworkType(mPhone.getContext(), 0); // wink FIXME: need to get SubId from somewhere.
     }
 
     /**
