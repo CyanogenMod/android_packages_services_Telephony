CyanogenMod/android_packages_services_Telephony

commit a4ae7bacf1ec564343540f3687505f3911796946

telephony: Fix package name warnings
Change-Id: Id229351301d16c687c0d19d19d417c19326bc89c

src/com/android/phone/CallFeaturesSetting.java
@@ -17,13 +17,10 @@
 
 package com.android.phone;
 
-import android.app.ActionBar;
 import android.app.Activity;
-import android.app.ActivityOptions;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.ProgressDialog;
-import android.content.ContentResolver;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -39,8 +36,8 @@ import android.os.AsyncResult;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
-import android.os.UserHandle;
 import android.os.SystemProperties;
+import android.os.UserHandle;
 import android.preference.CheckBoxPreference;
 import android.preference.ListPreference;
 import android.preference.Preference;
@@ -49,10 +46,9 @@ import android.preference.PreferenceManager;
 import android.preference.PreferenceScreen;
 import android.provider.ContactsContract.CommonDataKinds;
 import android.provider.Settings;
-import android.telecom.PhoneAccountHandle;
 import android.telecom.TelecomManager;
-import android.telephony.TelephonyManager;
 import android.telephony.PhoneNumberUtils;
+import android.telephony.TelephonyManager;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.MenuItem;
@@ -65,10 +61,11 @@ import com.android.internal.telephony.CallForwardInfo;
 import com.android.internal.telephony.CommandsInterface;
 import com.android.internal.telephony.Phone;
 import com.android.internal.telephony.PhoneConstants;
+import com.android.internal.telephony.util.BlacklistUtils;
 import com.android.phone.common.util.SettingsUtil;
+import com.android.phone.msim.SelectSubscription;
 import com.android.phone.settings.AccountSelectionPreference;
 import com.android.services.telephony.sip.SipUtil;
-import com.android.internal.telephony.util.BlacklistUtils;
 
 import java.util.Collection;
 import java.util.HashMap;

src/com/android/phone/EditPhoneNumberPreference.java
@@ -94,7 +94,7 @@ public class EditPhoneNumberPreference extends EditTextPreference {
      * DialogPreference.onDialogClosed(), except we also pass in a buttonClicked
      * value indicating which of the three possible buttons were pressed.
      */
-    interface OnDialogClosedListener {
+    public interface OnDialogClosedListener {
         void onDialogClosed(EditPhoneNumberPreference preference, int buttonClicked);
     }
 
@@ -102,7 +102,7 @@ public class EditPhoneNumberPreference extends EditTextPreference {
      * Interface for the default number setting listener.  Handles requests for
      * the default display number for the dialog.
      */
-    interface GetDefaultNumberListener {
+    public interface GetDefaultNumberListener {
         /**
          * Notify that we are looking for a default display value.
          * @return null if there is no contribution from this interface,

src/com/android/phone/PhoneGlobals.java
@@ -82,7 +82,7 @@ public class PhoneGlobals extends ContextWrapper {
      *
      * ***** DO NOT SUBMIT WITH DBG_LEVEL > 0 *************
      */
-    /* package */ static final int DBG_LEVEL = 2;
+    /* package */ public static final int DBG_LEVEL = 2;
 
     private static final boolean DBG =
             (PhoneGlobals.DBG_LEVEL >= 1) && (SystemProperties.getInt("ro.debuggable", 0) == 1);

src/com/android/phone/ims/ImsEditor.java
@@ -17,7 +17,7 @@
  * limitations under the License.
  */
 
-package com.android.phone;
+package com.android.phone.ims;
 
 import android.os.Bundle;
 import android.preference.ListPreference;

src/com/android/phone/msim/MSimCallFeaturesSubSetting.java
@@ -17,7 +17,9 @@
  * limitations under the License.
  */
 
-package com.android.phone;
+package com.android.phone.msim;
+
+import static com.android.internal.telephony.PhoneConstants.SUBSCRIPTION_KEY;
 
 import android.app.ActionBar;
 import android.app.Activity;
@@ -48,7 +50,6 @@ import android.preference.PreferenceManager;
 import android.preference.PreferenceScreen;
 import android.provider.ContactsContract.CommonDataKinds;
 import android.provider.Settings;
-import android.provider.Settings.SettingNotFoundException;
 import android.telephony.PhoneNumberUtils;
 import android.text.TextUtils;
 import android.util.Log;
@@ -62,17 +63,21 @@ import com.android.internal.telephony.CallForwardInfo;
 import com.android.internal.telephony.CommandsInterface;
 import com.android.internal.telephony.Phone;
 import com.android.internal.telephony.PhoneConstants;
+import com.android.phone.CallFeaturesSetting;
+import com.android.phone.Constants;
+import com.android.phone.DefaultRingtonePreference;
+import com.android.phone.EditPhoneNumberPreference;
+import com.android.phone.PhoneGlobals;
+import com.android.phone.PhoneUtils;
+import com.android.phone.R;
 import com.android.phone.common.util.SettingsUtil;
 
-import java.lang.String;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-
-import static com.android.internal.telephony.PhoneConstants.SUBSCRIPTION_KEY;
 
 /**
  * Top level "MSim Call settings" UI; see res/xml/call_feature_setting_msim.xml

src/com/android/phone/msim/MSimMobileNetworkSubSettings.java
@@ -17,7 +17,7 @@
  * limitations under the License.
  */
 
-package com.android.phone;
+package com.android.phone.msim;
 
 import android.app.ActionBar;
 import android.app.AlertDialog;
@@ -25,7 +25,6 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.net.ConnectivityManager;
 import android.os.AsyncResult;
 import android.os.Bundle;
 import android.os.Handler;
@@ -42,9 +41,13 @@ import android.view.MenuItem;
 
 import com.android.internal.telephony.Phone;
 import com.android.internal.telephony.PhoneConstants;
-import com.android.internal.telephony.PhoneFactory;
 import com.android.internal.telephony.TelephonyIntents;
 import com.android.internal.telephony.TelephonyProperties;
+import com.android.phone.CdmaOptions;
+import com.android.phone.EmergencyCallbackModeExitDialog;
+import com.android.phone.GsmUmtsOptions;
+import com.android.phone.PhoneGlobals;
+import com.android.phone.PhoneUtils;
 import com.android.phone.R;
 
 

src/com/android/phone/msim/SelectSubscription.java
@@ -27,23 +27,22 @@
  * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-package com.android.phone;
+package com.android.phone.msim;
 
+import android.app.TabActivity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.preference.Preference;
-import android.preference.PreferenceActivity;
-import android.preference.PreferenceScreen;
 import android.telephony.SubInfoRecord;
 import android.telephony.SubscriptionManager;
 import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.widget.TabHost;
 import android.widget.TabHost.TabSpec;
-import android.app.TabActivity;
-import java.util.List;
 
-import static com.android.internal.telephony.PhoneConstants.SUBSCRIPTION_KEY;
+import com.android.phone.PhoneGlobals;
+import com.android.phone.R;
+
+import java.util.List;
 
 public class SelectSubscription extends  TabActivity {
 

commit 91e66c9ee3e95a6beea36df35c3311e9cb208041

Telephony: fix MSim package names
Broke in a4ae7bacf1ec564343540f3687505f3911796946

Change-Id: I77072846a3db561521cca8bbb5e8b9e20dcc3232

AndroidManifest.xml
@@ -441,7 +441,7 @@
         </activity>
 
         <!-- MSim Call Feature Sub Settings -->
-        <activity android:name="MSimCallFeaturesSubSetting"
+        <activity android:name=".msim.MSimCallFeaturesSubSetting"
             android:label="@string/call_settings"
             android:configChanges="orientation|keyboardHidden"
             android:theme="@style/SettingsLight">
@@ -453,7 +453,7 @@
         </activity>
 
        <!-- MSim Mobile Network Settings per subscription -->
-       <activity android:name="MSimMobileNetworkSubSettings"
+       <activity android:name=".msim.MSimMobileNetworkSubSettings"
                 android:label="@string/settings_label"
                 android:theme="@style/SettingsLight">
             <intent-filter>
@@ -464,7 +464,7 @@
         </activity>
 
         <!-- Select Subscription -->
-        <activity android:name="SelectSubscription"
+        <activity android:name=".msim.SelectSubscription"
             android:label="@string/callind_multi_sim"
             android:configChanges="orientation|keyboardHidden"
             android:theme="@style/SettingsLight">
@@ -570,7 +570,7 @@
                 android:uiOptions="splitActionBarWhenNarrow">
         </activity>
 
-        <activity android:name="ImsEditor"
+        <activity android:name=".ims.ImsEditor"
                 android:theme="@style/DialerSettingsLight"
                 android:configChanges="orientation|screenSize|keyboardHidden"
                 android:uiOptions="splitActionBarWhenNarrow">

res/xml/call_feature_setting.xml
@@ -147,7 +147,7 @@
 
         <intent android:action="android.intent.action.MAIN"
             android:targetPackage="com.android.phone"
-            android:targetClass="com.android.phone.ImsEditor" />
+            android:targetClass="com.android.phone.ims.ImsEditor" />
     </PreferenceScreen>
 
     <PreferenceScreen

res/xml/call_feature_setting_msim.xml
@@ -28,7 +28,7 @@
 
       <intent android:action="android.intent.action.MAIN"
           android:targetPackage="com.android.phone"
-          android:targetClass="com.android.phone.SelectSubscription" />
+          android:targetClass="com.android.phone.msim.SelectSubscription" />
   </PreferenceScreen>
 
   <PreferenceCategory

src/com/android/phone/CallFeaturesSetting.java
@@ -1751,7 +1751,7 @@ public class CallFeaturesSetting extends PreferenceActivity
             Intent intent = selectSub.getIntent();
             intent.putExtra(SelectSubscription.PACKAGE, "com.android.phone");
             intent.putExtra(SelectSubscription.TARGET_CLASS,
-                    "com.android.phone.MSimCallFeaturesSubSetting");
+                    "com.android.phone.msim.MSimCallFeaturesSubSetting");
         }
 
         if (isMsim && TelephonyManager.getDefault().getMultiSimConfiguration() !=