5364d1ccba61ac0a75247251b509b26860ca4358
Pawit Pornkitprasan <p.pawit@gmail.com>
2014-12-24 19:38:40 +0700

    Revert "XDivert changes for MSIM"
    This reverts commit fa66d7833488cff0c0c6a69c92e87e2fc78af894.
    
    XDivert links to some package we don't have, leading to a crash.
    
    Change-Id: I740594dfbb1700fa45bf92dcdad4d23a03229b72

--- src/com/android/phone/CallFeaturesSetting.java
+++ src/com/android/phone/CallFeaturesSetting.java
@@ -191,7 +191,6 @@ public class CallFeaturesSetting extends PreferenceActivity
             "phone_account_settings_preference_screen";
 
     private static final String BUTTON_SELECT_SUB_KEY  = "button_call_independent_serv";
-    private static final String BUTTON_XDIVERT_KEY = "button_xdivert";
 
     private Intent mContactListIntent;
 
@@ -1752,15 +1751,6 @@ public class CallFeaturesSetting extends PreferenceActivity
             intent.putExtra(SelectSubscription.PACKAGE, "com.android.phone");
             intent.putExtra(SelectSubscription.TARGET_CLASS,
                     "com.android.phone.msim.MSimCallFeaturesSubSetting");
-        }
-
-        if (isMsim && TelephonyManager.getDefault().getMultiSimConfiguration() !=
-                TelephonyManager.MultiSimVariants.DSDS) {
-            PreferenceScreen mXDivertPref = (PreferenceScreen) findPreference(BUTTON_XDIVERT_KEY);
-            if (mXDivertPref != null) {
-                log("Remove xdivert preference");
-                prefSet.removePreference(mXDivertPref);
-            }
         }
 
         if (mButtonDTMF != null) {
