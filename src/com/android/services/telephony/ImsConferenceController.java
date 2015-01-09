4991b55cbf8790a622ece2321762ec7bd8bb0513
Omkar Kolangade <omkark@codeaurora.org>
2015-01-08 16:09:58 -0800

    IMS Conference Creation Update
    1. Notify the current state of a newly-created
    ImsConference to the upper layers for the UI
    to correctly display the Manage Conference
    button, along with the participants.
    
    2. Add the conference to Connection Service after
    adding child connections if any, to the conference.
    
    Change-Id: I1dfab033ff130f0d2ae74e3868f7438cae79fad5
    CRs-Fixed: 775146

--- src/com/android/services/telephony/ImsConferenceController.java
+++ src/com/android/services/telephony/ImsConferenceController.java
@@ -266,8 +266,9 @@ public class ImsConferenceController {
         // Create conference and add to telecom
         ImsConference conference = new ImsConference(mConnectionService, originalConnection);
         conference.setState(connection.getState());
-        mConnectionService.addConference(conference);
         conference.addListener(mConferenceListener);
+        conference.updateConferenceStateAfterCreation();
+        mConnectionService.addConference(conference);
 
         // Cleanup TelephonyConnection which backed the original connection and remove from telecom.
         // Use the "Canceled" disconnect cause to ensure the call is not logged.
