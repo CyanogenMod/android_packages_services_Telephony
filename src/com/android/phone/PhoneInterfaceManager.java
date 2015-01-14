ac5a5523192d06f66a4194149ed588b97b5fc2d2
Sanket Padawe <sanketpadawe@google.com>
2015-01-14 09:53:20 -0800

    Add getDeviceId to PhoneInterfaceManager to give id independent of SubInfo
    Bug: 18780202
    Conflicts:
    	src/com/android/phone/PhoneInterfaceManager.java
    
    Change-Id: I35a2a9e15ec90cac879320c0c1005f065f1b4f0d

--- src/com/android/phone/PhoneInterfaceManager.java
+++ src/com/android/phone/PhoneInterfaceManager.java
@@ -2190,4 +2190,22 @@ public class PhoneInterfaceManager extends ITelephony.Stub {
         }
         return result;
     }
+
+    /**
+     * Returns the unique device ID of phone, for example, the IMEI for
+     * GSM and the MEID for CDMA phones. Return null if device ID is not available.
+     *
+     * <p>Requires Permission:
+     *   {@link android.Manifest.permission#READ_PHONE_STATE READ_PHONE_STATE}
+     */
+    @Override
+    public String getDeviceId() {
+        enforceReadPermission();
+        final Phone phone = PhoneFactory.getPhone(0);
+        if (phone != null) {
+            return phone.getDeviceId();
+        } else {
+            return null;
+        }
+    }
 }
