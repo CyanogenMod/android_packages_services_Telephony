0be1556daea91cca915c67ae84c640f8e7ad5c8b
Adnan Begovic <adnan@cyngn.com>
2015-02-11 15:01:44 -0800

    TelephonyService: Only add VideoCall settings if supported.
    Change-Id: Id1e77e9240279a8cabbe72f87e60f4baef555607

--- src/com/android/phone/CallFeaturesSetting.java
+++ src/com/android/phone/CallFeaturesSetting.java
@@ -1624,6 +1624,9 @@ public class CallFeaturesSetting extends PreferenceActivity
         } else {
             addPreferencesFromResource(R.xml.call_feature_setting);
         }
+        if (isVTSupported()) {
+            addPreferencesFromResource(R.xml.video_call_settings);
+        }
         initPhoneAccountPreferences();
 
         // get buttons
