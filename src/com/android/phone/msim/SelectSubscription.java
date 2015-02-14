3cdbcdf80a492b948c036e41c634c14989c4864a
emancebo <emancebo@cyngn.com>
2015-02-13 17:07:56 -0800

    Fix back button on SelectSubscription activity
    Change-Id: Ibb2bd3ddff7039d2b33b3fc9884cec777aee4e5a

--- src/com/android/phone/msim/SelectSubscription.java
+++ src/com/android/phone/msim/SelectSubscription.java
@@ -39,6 +39,7 @@ import android.telephony.SubscriptionInfo;
 import android.telephony.SubscriptionManager;
 import android.telephony.TelephonyManager;
 import android.util.Log;
+import android.view.MenuItem;
 import android.widget.TabHost;
 import android.widget.TabHost.TabSpec;
 
@@ -128,4 +129,14 @@ public class SelectSubscription extends TabActivity {
         Log.d(LOG_TAG, msg);
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 }
