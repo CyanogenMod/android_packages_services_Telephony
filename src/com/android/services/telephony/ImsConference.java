0c131d43e97f0916d53a28ab57cd14ae061a86e8
Muhammed Siju <msiju@codeaurora.org>
2015-02-17 18:16:05 +0530

    IMS: Set the participants to disconnected state before removing.
    If the connection is not in disconnected state while removing,
    CallsManager will just mark it as DISCONNECTED. Explicitly
    set the state to disconnected before removing so that the call
    is removed from CallsManager call list.
    
    Change-Id: Ie4bddfd4b0af43eb56a00bcfb8e49980f520f305
    CRs-Fixed: 795369

--- src/com/android/services/telephony/ImsConference.java
+++ src/com/android/services/telephony/ImsConference.java
@@ -530,6 +530,7 @@ public class ImsConference extends Conference {
 
             if (!participantUserEntities.contains(entry.getKey())) {
                 ConferenceParticipantConnection participant = entry.getValue();
+                participant.setDisconnected(new DisconnectCause(DisconnectCause.CANCELED));
                 removeConferenceParticipant(participant);
                 removeConnection(participant);
                 entryIterator.remove();
@@ -597,11 +598,11 @@ public class ImsConference extends Conference {
         for (ConferenceParticipantConnection connection :
                 mConferenceParticipantConnections.values()) {
 
-            removeConferenceParticipant(connection);
-
             // Mark disconnect cause as cancelled to ensure that the call is not logged in the
             // call log.
             connection.setDisconnected(new DisconnectCause(DisconnectCause.CANCELED));
+            removeConferenceParticipant(connection);
+
             connection.destroy();
         }
         mConferenceParticipantConnections.clear();
