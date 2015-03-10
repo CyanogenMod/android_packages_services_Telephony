21dc5235289a73c39b10e67c21af71c644c62d0f
Utkarsh Gupta <utkarsh.eminem@gmail.com>
2015-03-10 19:36:55 +0530

    Fix network mode on non-msim devices
    Change-Id: I2732b7299329a4de2613142afb7a1ada0e0b0c3b

--- src/com/android/phone/MobileNetworkSettings.java
+++ src/com/android/phone/MobileNetworkSettings.java
@@ -605,6 +605,9 @@ public class MobileNetworkSettings extends PreferenceActivity
             }  else if (mButtonEnabledNetworks != null) {
                 mButtonEnabledNetworks.setEnabled(false);
             }
+        } else {
+            mPhone.setPreferredNetworkType(modemNetworkMode, mHandler
+                        .obtainMessage(MyHandler.MESSAGE_SET_PREFERRED_NETWORK_TYPE));
         }
     }
 
