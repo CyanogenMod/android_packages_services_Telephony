417bcdec39528f717049bc2caa100013810f966e
Ravi Paluri <rpaluri@codeaurora.org>
2015-03-15 14:25:00 +0530

    Revert "Ims: Update conferenceHostConnection state for a conference call"
    If call is immediately dropped soon after initiating, <orignalConnection>
    is set updated to NULL as there are no more calls. Any access to this
    <orginalConnection> will then result in NPE.
    To fix this, revert commit 2e5895c75883ad258e87d69f80c4e756b8633e0d.
    
    Change-Id: Ia4e151525d5b265f48d1a915935bfa753f55d217
    CRs-Fixed: 806071

--- src/com/android/services/telephony/TelephonyConnection.java
+++ src/com/android/services/telephony/TelephonyConnection.java
@@ -686,7 +686,6 @@ abstract class TelephonyConnection extends Connection {
 
         // Set video state and capabilities
         setVideoState(mOriginalConnection.getVideoState());
-        updateState();
         setLocalVideoCapable(mOriginalConnection.isLocalVideoCapable());
         setRemoteVideoCapable(mOriginalConnection.isRemoteVideoCapable());
         setVideoProvider(mOriginalConnection.getVideoProvider());
