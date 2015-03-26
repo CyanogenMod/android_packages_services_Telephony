ce9901a7f8abcb5dfa2744eb5a42004a391716c4
Matt Filetto <matt.filetto@gmail.com>
2015-03-25 23:41:31 -0700

    msim: fix CallFeatureSetting.java compile error
    Change-Id: I17bf8622a8f9a63afa4d00eb4a9ade62b85feb1b

--- src/com/android/phone/msim/MSimCallFeaturesSubSetting.java
+++ src/com/android/phone/msim/MSimCallFeaturesSubSetting.java
@@ -1627,7 +1627,7 @@ public class MSimCallFeaturesSubSetting extends PreferenceActivity
                             mPhone.getContext(),
                             mRingtoneLookupComplete,
                             RingtoneManager.TYPE_RINGTONE,
-                            mRingtonePreference,
+                            mRingtonePreference.getKey(),
                             MSG_UPDATE_RINGTONE_SUMMARY, mPhone.getPhoneId());
                 }
             }
