5ba2180d84ff31c80d33b0bbb299eed8f6c63ee9
Garik Badalyan <garikb@codeaurora.org>
2015-03-31 17:48:29 -0700

    IMS: Update conferenceHostConnection state for a conference call
    TelephonyConnection that backed the original connection is
    cleaned up after making a clone this TelephonyConnection that
    is eventually treated as conferenceHostConnection. Update
    conferenceHostConnection state too
    
    Change-Id: I91f856281bf03501b1d5c9df34a1b27f6dae43d8
    CRs-Fixed: 814549

--- src/com/android/services/telephony/TelephonyConnection.java
+++ src/com/android/services/telephony/TelephonyConnection.java
@@ -686,6 +686,9 @@ abstract class TelephonyConnection extends Connection {
 
         // Set video state and capabilities
         setVideoState(mOriginalConnection.getVideoState());
+        if (mOriginalConnection.isAlive()) {
+            updateState();
+        }
         setLocalVideoCapable(mOriginalConnection.isLocalVideoCapable());
         setRemoteVideoCapable(mOriginalConnection.isRemoteVideoCapable());
         setVideoProvider(mOriginalConnection.getVideoProvider());
