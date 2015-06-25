05923b17c9653e9b08184a52361209c820bc5bef
Chirayu Desai <chirayudesai1@gmail.com>
2015-06-25 17:54:11 +0530

    Remove network mode handling for other SIM
    * Follow up patch will not allow mode to be set for the non active data sub.
      See: Iec807ca1cd17d433f330484c4c3212246621e45c
        "For non-data SIMs, only set user configured mode, not preferred mode."
    
    * Any mode changes as needed when the default data sub changes will by
      handled by the framework in yet another patch
      See: I5c753cd5cfd8a4f832b18fd488a080810763c283
        "SubscriptionController: Change network mode on data sub change"
    
    Change-Id: Ia4dec96f2f3b212575e31cb34972c4e6cc5a97fa

--- src/com/android/phone/MobileNetworkSettings.java
+++ src/com/android/phone/MobileNetworkSettings.java
@@ -21,7 +21,6 @@ import com.android.ims.ImsManager;
 import com.android.ims.ImsException;
 import com.android.internal.telephony.Phone;
 import com.android.internal.telephony.PhoneConstants;
-import com.android.internal.telephony.PhoneFactory;
 import com.android.internal.telephony.TelephonyIntents;
 import com.android.internal.telephony.TelephonyProperties;
 
@@ -851,37 +850,20 @@ public class MobileNetworkSettings extends PreferenceActivity
     }
 
     private void setPreferredNetworkType(int modemNetworkMode) {
-        TelephonyManager tm = (TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
-        if (tm.isMultiSimEnabled()  &&
-                tm.getMultiSimConfiguration() == TelephonyManager.MultiSimVariants.DSDS &&
-                !SystemProperties.getBoolean("ro.ril.multi_rat_capable", false)) {
-            for (int i = 0; i < tm.getPhoneCount(); i++) {
-                if (mPhone.getPhoneId() != i) {
-                    Phone phone = PhoneFactory.getPhone(i);
-                    phone.setPreferredNetworkType(Phone.NT_MODE_GSM_ONLY, mHandler
-                            .obtainMessage(MyHandler.MESSAGE_SET_PREFERRED_NETWORK_TYPE_OTHER_SIM));
-                }
-            }
-            TelephonyManager.putIntAtIndex(mPhone.getContext().getContentResolver(),
-                    android.provider.Settings.Global.PREFERRED_NETWORK_MODE,
-                    mPhone.getPhoneId(),
-                    modemNetworkMode);
-            if (mButtonPreferredNetworkMode != null) {
-                mButtonPreferredNetworkMode.setEnabled(false);
-            }  else if (mButtonEnabledNetworks != null) {
-                mButtonEnabledNetworks.setEnabled(false);
-            }
-        } else {
-            mPhone.setPreferredNetworkType(modemNetworkMode, mHandler
-                        .obtainMessage(MyHandler.MESSAGE_SET_PREFERRED_NETWORK_TYPE));
+        if (mButtonPreferredNetworkMode != null) {
+            mButtonPreferredNetworkMode.setEnabled(false);
+        }  else if (mButtonEnabledNetworks != null) {
+            mButtonEnabledNetworks.setEnabled(false);
         }
+
+        mPhone.setPreferredNetworkType(modemNetworkMode, mHandler
+                .obtainMessage(MyHandler.MESSAGE_SET_PREFERRED_NETWORK_TYPE));
     }
 
     private class MyHandler extends Handler {
 
         static final int MESSAGE_GET_PREFERRED_NETWORK_TYPE = 0;
         static final int MESSAGE_SET_PREFERRED_NETWORK_TYPE = 1;
-        static final int MESSAGE_SET_PREFERRED_NETWORK_TYPE_OTHER_SIM = 2;
 
         @Override
         public void handleMessage(Message msg) {
@@ -892,22 +874,6 @@ public class MobileNetworkSettings extends PreferenceActivity
 
                 case MESSAGE_SET_PREFERRED_NETWORK_TYPE:
                     handleSetPreferredNetworkTypeResponse(msg);
-                    break;
-                case MESSAGE_SET_PREFERRED_NETWORK_TYPE_OTHER_SIM:
-                    int networkMode;
-                    if (mButtonPreferredNetworkMode != null) {
-                        networkMode = Integer.valueOf(
-                                mButtonPreferredNetworkMode.getValue()).intValue();
-                    } else if (mButtonEnabledNetworks != null) {
-                        networkMode = Integer.valueOf(
-                                mButtonEnabledNetworks.getValue()).intValue();
-                    } else {
-                        networkMode = -1;
-                    }
-                    if (networkMode != -1) {
-                        mPhone.setPreferredNetworkType(networkMode, mHandler
-                                .obtainMessage(MyHandler.MESSAGE_SET_PREFERRED_NETWORK_TYPE));
-                    }
                     break;
             }
         }
