a7d5b8002f5cc4b1b3fe5e70adca7767df64246b
Suresh Kumar Sugguna <sureshk@codeaurora.org>
2015-08-31 15:29:52 +0530

    IMS: Deflect call feature
    Add dummy listener funtions to avoid compilation errors.
    
    Change-Id: Id08456c74b39ecdbc9c2fc7772d1fbd7331682b7
    CRs-Fixed: 713856

--- src/com/android/phone/CallForwardEditPreference.java
+++ src/com/android/phone/CallForwardEditPreference.java
@@ -299,6 +299,10 @@ public class CallForwardEditPreference extends EditPhoneNumberPreference {
 
         public void onGetPacketErrorCount(int status, long packetErrorCount) {
         }
+
+        public void receiveCallDeflectResponse(int result) {
+            /* Not implemented, dummy implementation to avoid compilation errors */
+        }
     };
 
     private void handleGetCFTimerResponse() {
