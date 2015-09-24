9a499e61777be63c302afa1ed3d9887f56186c82
Ravindra <c_rthat@codeaurora.org>
2015-09-24 17:26:35 +0530

    Reset CFI on sim deactivation
    When the sim is deactivated update the call forwarding indicator
    icon on UI.
    
    Change-Id: I839929c738aa71755f1b1f71301788968c21762c
    CRs-Fixed: 778845

--- src/com/android/phone/CallNotifier.java
+++ src/com/android/phone/CallNotifier.java
@@ -24,6 +24,7 @@ import com.android.internal.telephony.Connection;
 import com.android.internal.telephony.Phone;
 import com.android.internal.telephony.PhoneConstants;
 import com.android.internal.telephony.PhoneBase;
+import com.android.internal.telephony.SubscriptionController;
 import com.android.internal.telephony.TelephonyCapabilities;
 import com.android.internal.telephony.cdma.CdmaInformationRecords.CdmaDisplayInfoRec;
 import com.android.internal.telephony.cdma.CdmaInformationRecords.CdmaSignalInfoRec;
@@ -34,6 +35,9 @@ import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothHeadset;
 import android.bluetooth.BluetoothProfile;
 import android.content.Context;
+import android.content.BroadcastReceiver;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.media.AudioAttributes;
 import android.media.AudioManager;
 import android.media.ToneGenerator;
@@ -89,6 +93,11 @@ public class CallNotifier extends Handler {
     private static final int CALLERINFO_QUERY_READY = 0;
     private static final int CALLERINFO_QUERYING = -1;
 
+    private static final String ACTION_UICC_MANUAL_PROVISION_STATUS_CHANGED =
+            "org.codeaurora.intent.action.ACTION_UICC_MANUAL_PROVISION_STATUS_CHANGED";
+    private static final String EXTRA_NEW_PROVISION_STATE = "newProvisionState";
+    private static final int NOT_PROVISIONED = 0;
+
     // the state of the CallerInfo Query.
     private int mCallerInfoQueryState;
 
@@ -96,7 +105,7 @@ public class CallNotifier extends Handler {
     private Object mCallerInfoQueryStateGuard = new Object();
     private Map<Integer, CallNotifierPhoneStateListener> mPhoneStateListeners =
             new ArrayMap<Integer, CallNotifierPhoneStateListener>();
-
+    private Map<Integer, Boolean> mCFIStatus = new ArrayMap<Integer, Boolean>();
     private PhoneGlobals mApplication;
     private CallManager mCM;
     private BluetoothHeadset mBluetoothHeadset;
@@ -117,6 +126,28 @@ public class CallNotifier extends Handler {
     private final BluetoothManager mBluetoothManager;
     private SubscriptionManager mSubscriptionManager;
     private TelephonyManager mTelephonyManager;
+
+    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            Log.d(LOG_TAG, "Intent received: " + action);
+            if (ACTION_UICC_MANUAL_PROVISION_STATUS_CHANGED.equals(action)) {
+                int slotId = intent.getIntExtra(PhoneConstants.PHONE_KEY,
+                        SubscriptionManager.INVALID_SIM_SLOT_INDEX);
+                int newProvisionedState = intent.getIntExtra(EXTRA_NEW_PROVISION_STATE,
+                        NOT_PROVISIONED);
+                 Log.d(LOG_TAG, "Received ACTION_UICC_MANUAL_PROVISION_STATUS_CHANGED on slotId: "
+                         + slotId + " new sub state " + newProvisionedState);
+                int subId[] = SubscriptionController.getInstance().getSubIdUsingSlotId(slotId);
+                if (mCFIStatus.containsKey(subId[0]) && (mCFIStatus.get(subId[0]) == true) &&
+                    newProvisionedState == NOT_PROVISIONED) {
+                    Log.d(LOG_TAG, "SubId: " +subId[0]+" "+"NOT_PROVISIONED");
+                    mApplication.notificationMgr.updateCfi(subId[0], false);
+                }
+            }
+        }
+    };
 
     /**
      * Initialize the singleton CallNotifier instance.
@@ -170,6 +201,8 @@ public class CallNotifier extends Handler {
                         updatePhoneStateListeners();
                     }
                 });
+        IntentFilter intentFilter = new IntentFilter(ACTION_UICC_MANUAL_PROVISION_STATUS_CHANGED);
+        mApplication.getApplicationContext().registerReceiver(mReceiver, intentFilter);
     }
 
     private void createSignalInfoToneGenerator() {
@@ -1185,6 +1218,7 @@ public class CallNotifier extends Handler {
         @Override
         public void onCallForwardingIndicatorChanged(boolean visible) {
             if (VDBG) log("onCallForwardingIndicatorChanged(): " + this.mSubId + " " + visible);
+            mCFIStatus.put(this.mSubId, visible);
             mApplication.notificationMgr.updateCfi(this.mSubId, visible);
         }
     };
