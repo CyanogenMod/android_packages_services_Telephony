74d99b69619ffec6205efb36f21ed243f2a89b63
Benergy Meenan Ravuri <bravuri@codeaurora.org>
2015-12-02 12:13:03 +0530

    IMS: Remove the methods with dummy implementation
    Since the base implementation is added for the listener, the methods
    with dummy implementation can be removed.
    
    Change-Id: I7a1c9e4f44d20eab3792923ce9877d0f5188467f
    CRs-Fixed: 901766

--- src/com/android/phone/CallForwardEditPreference.java
+++ src/com/android/phone/CallForwardEditPreference.java
@@ -29,6 +29,7 @@ import android.view.View;
 
 import org.codeaurora.ims.qtiims.IQtiImsInterfaceListener;
 import org.codeaurora.ims.qtiims.IQtiImsInterface;
+import org.codeaurora.ims.qtiims.QtiImsInterfaceListenerBaseImpl;
 
 import static com.android.phone.TimeConsumingPreferenceActivity.RESPONSE_ERROR;
 import static com.android.phone.TimeConsumingPreferenceActivity.EXCEPTION_ERROR;
@@ -264,7 +265,10 @@ public class CallForwardEditPreference extends EditPhoneNumberPreference {
 
     }
 
-    private IQtiImsInterfaceListener imsInterfaceListener = new IQtiImsInterfaceListener.Stub() {
+    private QtiImsInterfaceListenerBaseImpl imsInterfaceListener =
+            new QtiImsInterfaceListenerBaseImpl() {
+
+        @Override
         public void onSetCallForwardUncondTimer(int status) {
             if (DBG) Log.d(LOG_TAG, "onSetCallForwardTimer status= "+status);
             try {
@@ -276,6 +280,7 @@ public class CallForwardEditPreference extends EditPhoneNumberPreference {
             }
         }
 
+        @Override
         public void onGetCallForwardUncondTimer(int startHour, int endHour, int startMinute,
                 int endMinute, int reason, int status, String number, int service) {
             Log.d(LOG_TAG,"onGetCallForwardUncondTimer startHour= " + startHour + " endHour = "
@@ -291,20 +296,11 @@ public class CallForwardEditPreference extends EditPhoneNumberPreference {
             handleGetCFTimerResponse();
         }
 
+        @Override
         public void onUTReqFailed(int errCode, String errString) {
             if (DBG) Log.d(LOG_TAG, "onUTReqFailed errCode= "+errCode + "errString ="+ errString);
             mTcpListener.onFinished(CallForwardEditPreference.this, true);
             mTcpListener.onError(CallForwardEditPreference.this, RESPONSE_ERROR);
-        }
-
-        public void onGetPacketCount(int status, long packetCount) {
-        }
-
-        public void onGetPacketErrorCount(int status, long packetErrorCount) {
-        }
-
-        public void receiveCallDeflectResponse(int result) {
-            /* Not implemented, dummy implementation to avoid compilation errors */
         }
     };
 
