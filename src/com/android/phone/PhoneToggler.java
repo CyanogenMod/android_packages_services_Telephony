e6e8a6d03d5f697e61b99f9b1330b9508007dbe1
Roman Birg <roman@cyngn.com>
2016-02-09 18:13:03 -0800

    Telephony: add external network selection activity
    Instead of trying to recreate the logic inside the profile
    configuration, allow the profile setup to ask for these and return the
    result.
    
    Ref: CYNGNOS-1463
    
    Change-Id: I30d46b475a1bf11bd3360059c00c3b6602fc3759
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- src/com/android/phone/PhoneToggler.java
+++ src/com/android/phone/PhoneToggler.java
@@ -19,18 +19,14 @@ package com.android.phone;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.os.AsyncResult;
-import android.os.Handler;
-import android.os.Message;
-import android.provider.Settings;
 import android.telephony.SubscriptionManager;
 import android.util.Log;
 
 import com.android.internal.telephony.Phone;
-import com.android.internal.telephony.PhoneConstants;
 import com.android.internal.telephony.PhoneFactory;
+import com.android.internal.telephony.SubscriptionController;
 
-public class PhoneToggler extends BroadcastReceiver  {
+public class PhoneToggler extends BroadcastReceiver {
 
     @Deprecated
     public static final String ACTION_REQUEST_NETWORK_MODE =
@@ -39,18 +35,15 @@ public class PhoneToggler extends BroadcastReceiver  {
     public static final String ACTION_MODIFY_NETWORK_MODE =
             "com.android.internal.telephony.MODIFY_NETWORK_MODE";
 
+    public static final String EXTRA_SUB_ID = "subId";
+
     public static final String EXTRA_NETWORK_MODE = "networkMode";
 
     public static final String CHANGE_NETWORK_MODE_PERM =
             "com.android.phone.CHANGE_NETWORK_MODE";
 
     private static final String LOG_TAG = "PhoneToggler";
-    private static final boolean DBG = false;
-
-    private Phone getPhone() {
-        return PhoneFactory.getPhone(SubscriptionManager.getPhoneId(
-                SubscriptionManager.getDefaultDataSubId()));
-    }
+    private static final boolean DBG = true;
 
     @Override
     public void onReceive(Context context, Intent intent) {
@@ -58,49 +51,73 @@ public class PhoneToggler extends BroadcastReceiver  {
         if (ACTION_MODIFY_NETWORK_MODE.equals(action)) {
             if (DBG) Log.d(LOG_TAG, "Got modify intent");
             if (intent.getExtras() != null) {
-                int networkMode = intent.getExtras().getInt(EXTRA_NETWORK_MODE);
-                boolean networkModeOk = false;
-                Phone phone = getPhone();
-                int phoneType = phone.getPhoneType();
-                boolean isLteOnCdma = phone.getLteOnCdmaMode() == PhoneConstants.LTE_ON_CDMA_TRUE;
+                SubscriptionController subCtrl = SubscriptionController.getInstance();
 
-                if (phoneType == PhoneConstants.PHONE_TYPE_GSM) {
-                    if (networkMode == Phone.NT_MODE_GSM_ONLY
-                            || networkMode == Phone.NT_MODE_GSM_UMTS
-                            || networkMode == Phone.NT_MODE_WCDMA_PREF
-                            || networkMode == Phone.NT_MODE_LTE_GSM_WCDMA
-                            || networkMode == Phone.NT_MODE_WCDMA_ONLY) {
-                        networkModeOk = true;
-                    }
-                } else if (phoneType == PhoneConstants.PHONE_TYPE_CDMA) {
-                    if (networkMode == Phone.NT_MODE_CDMA
-                            || networkMode == Phone.NT_MODE_CDMA_NO_EVDO
-                            || networkMode == Phone.NT_MODE_EVDO_NO_CDMA) {
-                        networkModeOk = true;
-                    }
-                }
-                if (context.getResources().getBoolean(R.bool.world_phone) || isLteOnCdma) {
-                    if (networkMode == Phone.NT_MODE_GLOBAL) {
-                        networkModeOk = true;
-                    }
-                }
+                int networkMode = intent.getExtras().getInt(EXTRA_NETWORK_MODE, -1);
+                int subId = intent.getExtras().getInt(EXTRA_SUB_ID,
+                        SubscriptionManager.getDefaultDataSubId());
 
-                if (networkModeOk) {
+                // since the caller must be a system app, it's assumed that they have already
+                // chosen a valid network mode for this subId, so only basic validation is done
+                if (isValidModemNetworkMode(networkMode)) {
                     if (DBG) Log.d(LOG_TAG, "Changing network mode to " + networkMode);
-                    changeNetworkMode(networkMode);
+                    subCtrl.setUserNwMode(subId, networkMode);
+                    try {
+                        PhoneFactory.getPhone(SubscriptionManager.getPhoneId(subId))
+                                .setPreferredNetworkType(networkMode, null);
+                    } catch (Throwable t) {
+                        Log.d(LOG_TAG, "error setting preferred network", t);
+                    }
                 } else {
-                    Log.e(LOG_TAG,"Invalid network mode: "+networkMode);
+                    if (DBG) {
+                        String error = "invalid network mode (%d) or invalid subId (%d)";
+                        Log.d(LOG_TAG, String.format(error, networkMode, subId));
+                    }
                 }
             }
         } else if (ACTION_REQUEST_NETWORK_MODE.equals(action)) {
-            if (DBG) Log.e(LOG_TAG,"Requested network mode. Not honoring request. Get your own info.");
+            if (DBG) {
+                Log.e(LOG_TAG, "Requested network mode. Not honoring request. Get your own info.");
+            }
         } else {
-            if (DBG) Log.d(LOG_TAG,"Unexpected intent: " + intent);
+            if (DBG) {
+                Log.d(LOG_TAG, "Unexpected intent: " + intent);
+            }
         }
     }
 
-    private void changeNetworkMode(int modemNetworkMode) {
-        getPhone().setPreferredNetworkType(modemNetworkMode, null);
+    /*
+     * Basic validation; from MobileNetworkSettings
+     */
+    private static boolean isValidModemNetworkMode(int mode) {
+        switch (mode) {
+            case Phone.NT_MODE_WCDMA_PREF:
+            case Phone.NT_MODE_GSM_ONLY:
+            case Phone.NT_MODE_WCDMA_ONLY:
+            case Phone.NT_MODE_GSM_UMTS:
+            case Phone.NT_MODE_CDMA:
+            case Phone.NT_MODE_CDMA_NO_EVDO:
+            case Phone.NT_MODE_EVDO_NO_CDMA:
+            case Phone.NT_MODE_GLOBAL:
+            case Phone.NT_MODE_LTE_CDMA_AND_EVDO:
+            case Phone.NT_MODE_LTE_GSM_WCDMA:
+            case Phone.NT_MODE_LTE_CDMA_EVDO_GSM_WCDMA:
+            case Phone.NT_MODE_LTE_ONLY:
+            case Phone.NT_MODE_LTE_WCDMA:
+            case Phone.NT_MODE_TDSCDMA_ONLY:
+            case Phone.NT_MODE_TDSCDMA_WCDMA:
+            case Phone.NT_MODE_LTE_TDSCDMA:
+            case Phone.NT_MODE_TDSCDMA_GSM:
+            case Phone.NT_MODE_LTE_TDSCDMA_GSM:
+            case Phone.NT_MODE_TDSCDMA_GSM_WCDMA:
+            case Phone.NT_MODE_LTE_TDSCDMA_WCDMA:
+            case Phone.NT_MODE_LTE_TDSCDMA_GSM_WCDMA:
+            case Phone.NT_MODE_TDSCDMA_CDMA_EVDO_GSM_WCDMA:
+            case Phone.NT_MODE_LTE_TDSCDMA_CDMA_EVDO_GSM_WCDMA:
+                return true;
+            default:
+                return false;
+        }
     }
 
 }
