3b24ee887af034f4d1607dbeaef7091ade1f29d7
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2016-03-14 03:56:06 -0800

    Revert "Fix "multi-sim: fallback on active subId on emergency calls""
    This reverts commit df6ddbe1d75fab299ef8ca88ae00b97ead1cd035.
    
    Change-Id: I2b300d27f84be3489005f9630ed5cc0ec7c34599

--- src/com/android/services/telephony/TelephonyConnectionService.java
+++ src/com/android/services/telephony/TelephonyConnectionService.java
@@ -610,7 +610,6 @@ public class TelephonyConnectionService extends ConnectionService {
             } catch (NullPointerException ex) {
                 Log.e(this, ex, "Exception : " + ex);
             }
-            return PhoneFactory.getPhone(phoneId);
         }
 
         int subId = PhoneUtils.getSubIdForPhoneAccountHandle(accountHandle);
