fa77260affaeb1c628f84bd3d9228bf53f62e8c5
Brad Ebinger <breadley@google.com>
2016-05-03 16:01:01 -0700

    [PATCH] DO NOT MERGE Use E PhoneAccount for MT ECM Call
    Use the correct Emergency PhoneAccount for a MT call on a phone with no
    SIM that is in ECM. Also, reject incoming SIP calls that are received on
    a disabled PhoneAccount instead of crashing the Phone process.
    
    Ticket: CYNGNOS-2707
    Bug: 27820226
    Change-Id: I79aa1eb9d07760a6d31d97ec2e2cd3c430a43215
    (cherry picked from commit a1a6eb91f88bd6f670f8d5d76a5510f7b859d2fb)

--- src/com/android/phone/PhoneUtils.java
+++ src/com/android/phone/PhoneUtils.java
@@ -77,6 +77,7 @@ import java.util.List;
  * Misc utilities for the Phone app.
  */
 public class PhoneUtils {
+    public static final String EMERGENCY_ACCOUNT_HANDLE_ID = "E";
     private static final String LOG_TAG = "PhoneUtils";
     private static final boolean DBG = (PhoneGlobals.DBG_LEVEL >= 2);
 
@@ -2480,7 +2481,8 @@ public class PhoneUtils {
             Phone phone, String prefix, boolean isEmergency) {
         // TODO: Should use some sort of special hidden flag to decorate this account as
         // an emergency-only account
-        String id = isEmergency ? "E" : prefix + String.valueOf(phone.getSubId());
+        String id = isEmergency ? EMERGENCY_ACCOUNT_HANDLE_ID : prefix +
+                String.valueOf(phone.getSubId());
         return makePstnPhoneAccountHandleWithPrefix(id, prefix, isEmergency);
     }
 
