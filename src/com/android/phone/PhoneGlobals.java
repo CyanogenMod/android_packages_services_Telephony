8be01beb990167cc9853578615a8c4a7ebb6256d
leskal <alexakis1997@gmail.com>
2016-07-30 02:17:56 +0300

    mtk services Telephony
        Change-Id: I523b1909b72a0c1d331565f1cf13f45d2f67444c
    
        Changes by: Deepflex
    
    Change-Id: Ib5c7762d425b2d5b6d00304b0c70a22ad6ee99ac

--- src/com/android/phone/PhoneGlobals.java
+++ src/com/android/phone/PhoneGlobals.java
@@ -65,6 +65,8 @@ import com.android.phone.settings.SettingsConstants;
 import com.android.server.sip.SipService;
 import com.android.services.telephony.activation.SimActivationManager;
 
+import com.mediatek.phone.PhoneInterfaceManagerEx;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -207,6 +209,11 @@ public class PhoneGlobals extends ContextWrapper {
     public OtaUtils.CdmaOtaConfigData cdmaOtaConfigData;
     public OtaUtils.CdmaOtaScreenState cdmaOtaScreenState;
     public OtaUtils.CdmaOtaInCallScreenUiState cdmaOtaInCallScreenUiState;
+
+
+
+    // MTK
+    PhoneInterfaceManagerEx phoneMgrEx;
 
 
 
@@ -376,6 +383,7 @@ public class PhoneGlobals extends ContextWrapper {
             callStateMonitor = new CallStateMonitor(mCM);
 
             phoneMgr = PhoneInterfaceManager.init(this, PhoneFactory.getDefaultPhone());
+            phoneMgrEx = PhoneInterfaceManagerEx.init(this, PhoneFactory.getDefaultPhone());  // MTK
 
             configLoader = CarrierConfigLoader.init(this);
 
