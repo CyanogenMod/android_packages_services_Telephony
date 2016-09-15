e892d647220623169b7dc34725bcb1c6e3556856
Zhao Wei Liew <zhaoweiliew@gmail.com>
2016-09-15 18:46:53 +0800

    Telephony: Break qti-telephony-framework dependency
      Not all devices will have the qti-telephony-framework,
      so fall back to AOSP's methods when needed.
    
    Change-Id: Ia65f4bd3df1baecc0d558471fe7cdea1d1f2dcbd

--- src/com/android/phone/EmergencyDialer.java
+++ src/com/android/phone/EmergencyDialer.java
@@ -515,7 +515,7 @@ public class EmergencyDialer extends Activity implements View.OnClickListener,
      */
     private void placeCall() {
         mLastNumber = mDigits.getText().toString();
-        if (PhoneUtils.isLocalEmergencyNumber(mLastNumber)) {
+        if (PhoneUtils.isLocalEmergencyNumber(this, mLastNumber)) {
             if (DBG) Log.d(LOG_TAG, "placing call to " + mLastNumber);
 
             // place the call if it is a valid number
